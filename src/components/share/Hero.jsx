70a169f092429a795f54286200dade1d85a0cd81
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-04-28 16:03:09 +0600

    create footer

--- src/components/share/Hero.jsx
+++ src/components/share/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div className="bg-[url('https://i.pinimg.com/1200x/8a/fe/83/8afe83b98f339de4c1dd34fde26a86d0.jpg')] h-[60vh] w-full bg-cover bg-no-repeat bg-center flex items-center rounded-lg shadow-2xl">
+        <div className="bg-[url('https://i.pinimg.com/1200x/8a/fe/83/8afe83b98f339de4c1dd34fde26a86d0.jpg')] h-[60vh] w-full bg-cover bg-no-repeat bg-center flex items-center rounded-xl  shadow-2xl">
             <div className="p-10 space-y-5 text-white">
                 <h1 className=' text-3xl md:text-5xl font-bold'>Turn Ideas into Stunning AI Art</h1>
                 <p className='font-bold  text-lg'>Generate high-quality images from simple text prompts.
