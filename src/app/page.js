76e689b624ecf0a0b39367543e03d3603d7ce799
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-04-28 15:58:38 +0600

    create hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Hero from "@/components/share/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-red-300">
-      <h1>this is amin page heading</h1>
+      <Hero/>
     </div>
   );
 }
