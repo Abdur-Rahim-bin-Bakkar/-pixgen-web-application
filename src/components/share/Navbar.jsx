2cdb7ca4c8dfa8348e547a7745998a188f976734
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-04-28 15:46:10 +0600

    create navbar

--- src/components/share/Navbar.jsx
+++ src/components/share/Navbar.jsx
@@ -1,8 +1,28 @@
+import Image from 'next/image';
 import React from 'react';
+import logo from '@/assets/logo.png'
+import Link from 'next/link';
+import { Button } from '@heroui/react';
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='flex justify-between h-15 items-center px-5'>
+            <div className="flex gap-2">
+                <Image src={logo} alt='logo' height={30} width={30}/>
+                <h3 className='font-bold text-lg'>pixgen.</h3>
+            </div>
+
+            <div className="flex gap-5">
+                <Link className='font-medium' href={'/'}>Home</Link>
+                <Link className='font-medium' href={'/'}>Home</Link>
+                <Link className='font-medium' href={'/'}>Home</Link>
+                <Link className='font-medium' href={'/'}>Home</Link>
+            </div>
+
+            <div className="flex gap-3">
+                <Link className='font-medium ' href={'/signin'}><Button>Sign IN</Button></Link>
+                <Link className='font-medium' href={'/signup'}><Button className={'bg-indigo-500'}>Sign UP</Button></Link>
+            </div>
             
         </div>
     );
